c395f4c368e972cdcc739b2fff84995bb0102068
Bdzone43 <150906861+Bdzone43@users.noreply.github.com>
2024-10-25 21:23:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "123456"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
+API_ID = "23306415"  # Replace this API ID with your actual API ID
+API_HASH = "fecb61dcfee0e426cf6ef96cc6f32cbc"  # Replace this API HASH with your actual API HASH
 SESSION_STRING = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Replace this SESSION STRING with your actual SESSION_STRING
-BOT_TOKEN = "12345678:XXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+BOT_TOKEN = "7551762871:AAFERWb44JKStY5-rD4xGsD7IBPtByY1nuw"  # Replace this BOT_TOKEN
 
 # Admin IDs
 ADMIN_IDS = [12345678, 12345678]
