86c7d47672e627de9032c8ca1d214be8e15719eb
Bdzone43 <150906861+Bdzone43@users.noreply.github.com>
2024-10-25 21:26:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 # Channel: https://t.me/Xd_Cyber_world
 
 # Pyrogram setup
-API_ID = "22437715"  # Replace this API ID with your actual API ID
-API_HASH = "e516ca8cd543ccd603da05291d00da02"  # Replace this API HASH with your actual API HASH
+API_ID = "23306415"  # Replace this API ID with your actual API ID
+API_HASH = "fecb61dcfee0e426cf6ef96cc6f32cbc"  # Replace this API HASH with your actual API HASH
 SESSION_STRING = "AQFWX1MAARBw_DWm_CouvNuPLUhEWABY7eUkKDlUqmRPVlDAKt4gR5jnnzsU2_5svVjbv6Df3sqBAw1emE9YwDvxXvWd2DlINwPPL4_535u-ZCKSN7a2Um-pvZr4K1d5dEFkv6Fg2G9yQLkHkoI7Hf2FjZjzJmwqP_uvBIGTdQ41hS44PY4OcHwu4sEd5QZBJELQqTi7-hDG_oZXk3v-F66kkKf5h3stLdEgZ0pkQSuZpOtTfPWNDIVkeV146U5V0_3ko6TEu0prmCOICM6w_AyrqjYrCWKZh9Z6PdK3govFa-AjJG6Q8lp_lPh17wAIfd6Bxn4lHocmhDuUCA6ByE2ysJglDgAAAAGpIPzWAA"  # Replace this SESSION STRING with your actual SESSION_STRING
-BOT_TOKEN = "7538408613:AAGP1u1TycbG-Iluf5O-sKYGYab839kc-Is"  # Replace this BOT_TOKEN
+BOT_TOKEN = "7551762871:AAFERWb44JKStY5-rD4xGsD7IBPtByY1nuw"  # Replace this BOT_TOKEN
 
 # Admin IDs
 ADMIN_IDS = [7132478678, 1718615866]
